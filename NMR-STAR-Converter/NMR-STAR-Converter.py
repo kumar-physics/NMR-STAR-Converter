963dbc694de06c348e6746c0b48725af7015ea92
Kumaran Baskaran <kbaskaran2@wisc.edu>
2017-03-15 11:51:08 -0500

    initial commit

---
+++ NMR-STAR-Converter/NMR-STAR-Converter.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 15, 2017
+
+@author: kumaran
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
