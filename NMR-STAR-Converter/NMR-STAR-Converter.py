8b04334470f2ca43d0c4cd1568a6dc58d7d57bc4
Kumaran Baskaran <kbaskaran2@wisc.edu>
2017-03-30 15:14:36 -0500

    -working

--- NMR-STAR-Converter/NMR-STAR-Converter.py
+++ NMR-STAR-Converter/NMR-STAR-Converter.py
@@ -3,15 +3,50 @@ Created on Mar 15, 2017
 
 @author: kumaran
 '''
+import pynmrstar
 
-class MyClass(object):
+class NMRSTAR(object):
     '''
     classdocs
     '''
 
 
-    def __init__(self, params):
+    def __init__(self,filename):
         '''
         Constructor
         '''
+        self.inFile = filename
+        self.oldSchema = pynmrstar.Schema('/home/kumaran/nmrstar/adit_input/xlschem_ann.csv')
+        self.newSchema = pynmrstar.Schema()
         
+    
+    def Convert(self):
+        self.oldStar = pynmrstar.Entry.from_file('/home/kumaran/nmrstar/adit_input/nmrstar31.str')
+        self.newStar = pynmrstar.Entry.from_file('/home/kumaran/nmrstar/adit_input/nmrstar32.str')
+        self.oldTags = []
+        self.newTags = []
+        l1 = str(self.oldSchema).split("\n")
+        p=""
+        for x in l1:
+            if len(x)>0:
+                if x[0] == "_":
+                    p=x
+                else:
+                    self.oldTags.append("%s.%s"%(p.strip(),x.strip().split(" ")[0]))
+        l1 = str(self.newSchema).split("\n")
+        p=""
+        for x in l1:
+            if len(x)>0:
+                if x[0] == "_":
+                    p=x
+                else:
+                    self.newTags.append("%s.%s"%(p.strip(),x.strip().split(" ")[0]))
+        self.oldSet=set(self.oldTags)
+        self.newSet=set(self.newTags)
+        for x in self.newSet-self.oldSet:
+            print x
+if __name__=="__main__":
+    p = NMRSTAR('/home/kumaran/nmrstar/adit_input/nmrstar31.str')
+    p.Convert()
+    
+      
