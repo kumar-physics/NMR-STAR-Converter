0b4495a56c36be8076008ef2fe329ef70d2d6362
Kumaran Baskaran <kumar.physics@gmail.com>
2017-09-01 15:54:54 -0500

    big fixed need to write error handling

--- NMR-STAR-Converter/NMR-STAR-Converter.py
+++ NMR-STAR-Converter/NMR-STAR-Converter.py
@@ -17,18 +17,20 @@ class NMRSTAR(object):
         '''
         Constructor
         '''
-        self.inFile = filename
-        self.outfile = '/home/kumaran/git/NMR-STAR-Converter/output.str'
+        self.inFile = str(filename)
+        
         self.newSchema = pynmrstar.Schema('/home/kumaran/git/nmr-star-dictionary/bmrb_only_files/adit_input/xlschem_ann.csv')
         self.oldSchema = pynmrstar.Schema('/home/kumaran/git/nmr-star-dictionary/bmrb_star_v3_files/adit_input/xlschem_ann.csv')
         self.today = datetime.date.today()
     
     def Convert(self):
+        print "x",self.inFile,"x"
         self.inputStar = pynmrstar.Entry.from_database(self.inFile)
         #pynmrstar.validate(self.oldStar,self.newSchema)
         #self.newStar = pynmrstar.Entry.from_file('/home/kumaran/nmrstar/adit_input/nmrstar32.str')
         #self.ReadMapFile()
         self.outputStar = pynmrstar.Entry.from_scratch(self.inputStar.entry_id)
+        self.outfile = '/home/kumaran/git/NMR-STAR-Converter/Output/%s.str'%(self.inputStar.entry_id)
         for saveframe in self.inputStar:
             if saveframe.category == "entry_information":
                 outSaveframe = copy.deepcopy(saveframe)
@@ -81,11 +83,40 @@ class NMRSTAR(object):
                     else:
                         outSaveframe.add_tag(tag[0],tag[1])
                 outSaveframe["Text_data_format"]='.'
-                self.dim = atoi(saveframe["Number_of_spectral_dimensions"][0]) 
+                try:
+                    self.dim = atoi(saveframe["Number_of_spectral_dimensions"][0]) 
+                except ValueError:
+                    try:
+                        self.dim = max([atoi(d[saveframe.get_loop_by_category("_Spectral_dim").columns.index("ID")]) for d in saveframe.get_loop_by_category("_Spectral_dim").data])
+                    except KeyError:
+                        self.dim = max([atoi(d[saveframe.get_loop_by_category("_Peak_char").columns.index("Spectral_dim_ID")]) for d in saveframe.get_loop_by_category("_Peak_char").data])
                 self.loop_category = [loop.category for loop in saveframe]
-                peak = copy.deepcopy(saveframe.get_loop_by_category("_Peak"))
-                spect_dim = saveframe.get_loop_by_category("_Spectral_dim")
-                outSaveframe.add_loop(spect_dim)
+                if "_Peak" in self.loop_category:
+                    peak = copy.deepcopy(saveframe.get_loop_by_category("_Peak"))
+                else:
+                    peak = pynmrstar.Loop.from_scratch("_Peak")
+                    peak.add_column("ID")
+                    peak.add_column("Entry_ID")
+                    peak.add_column("Spectral_peak_list_ID")
+                    #print saveframe.name,self.loop_category
+                    if "_Peak_general_char" in self.loop_category:
+                        max_peaks = max([atoi(d[saveframe.get_loop_by_category("_Peak_general_char").columns.index("Peak_ID")]) for d in saveframe.get_loop_by_category("_Peak_general_char").data])
+                        peak_list_id = saveframe.get_loop_by_category("_Peak_general_char").data[0][saveframe.get_loop_by_category("_Peak_general_char").columns.index("Spectral_peak_list_ID")]
+                    elif "_Peak_char" in self.loop_category:
+                        max_peaks = max([atoi(d[saveframe.get_loop_by_category("_Peak_char").columns.index("Peak_ID")]) for d in saveframe.get_loop_by_category("_Peak_char").data])
+                        peak_list_id = saveframe.get_loop_by_category("_Peak_char").data[0][saveframe.get_loop_by_category("_Peak_char").columns.index("Spectral_peak_list_ID")]
+                    elif "_Assigned_peak_chem_shift" in self.loop_category:
+                        max_peaks = max([atoi(d[saveframe.get_loop_by_category("_Assigned_peak_chem_shift").columns.index("Peak_ID")]) for d in saveframe.get_loop_by_category("_Assigned_peak_chem_shift").data])
+                        peak_list_id = saveframe.get_loop_by_category("_Assigned_peak_chem_shift").data[0][saveframe.get_loop_by_category("_Assigned_peak_chem_shift").columns.index("Spectral_peak_list_ID")]
+                    else:
+                        print saveframe.name,self.loop_category
+                        print "Cann't find the Peak information"
+                        exit(1)
+                    for i in range(max_peaks):
+                        peak.add_data(["%d"%(i+1),self.inputStar.entry_id,peak_list_id])
+                if "_Spectral_dim" in self.loop_category: 
+                    spect_dim = saveframe.get_loop_by_category("_Spectral_dim")
+                    outSaveframe.add_loop(spect_dim)
                 if "_Peak_general_char" in self.loop_category:
                     peak_gen_char = saveframe.get_loop_by_category("_Peak_general_char")
                     #print len(peak_gen_char.get_data_by_tag("Measurement_method"))
@@ -104,7 +135,7 @@ class NMRSTAR(object):
                     peak_char = saveframe.get_loop_by_category("_Peak_char")
                     for i in range(self.dim):
                         peak.add_column("Position_%d"%(i+1))
-                        peak.add_column("Position_uncertainity_%d"%(i+1))
+                        peak.add_column("Position_uncertainty_%d"%(i+1))
                     position = {}
                     position_err = {}
                     for dat in peak_char.data:
@@ -128,6 +159,9 @@ class NMRSTAR(object):
                     if "Atom_ID" in assigned_peak.columns:
                         for i in range(self.dim):
                             peak.add_column("Atom_ID_%d"%(i+1))
+                    if "Auth_entity_assembly_ID" in assigned_peak.columns:
+                        for i in range(self.dim):
+                            peak.add_column("Auth_entity_assembly_ID_%d"%(i+1))
                     if "Auth_entity_ID" in assigned_peak.columns:
                         for i in range(self.dim):
                             peak.add_column("Auth_entity_ID_%d"%(i+1))
@@ -146,41 +180,46 @@ class NMRSTAR(object):
                     com_id = {}
                     com_ind_id = {}
                     atm_id = {}
+                    aut_ent_asm_id = {}
                     aut_ent_id = {}
                     aut_com_id = {}
                     aut_seq_id = {}
                     aut_atm_id = {}
                     for dat in assigned_peak.data:
-                        ent_asm_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Entity_assembly_ID")]
-                        ent_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Entity_ID")]
-                        com_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Comp_ID")] 
-                        com_ind_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Comp_index_ID")]
-                        atm_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Atom_ID")]
-                        aut_ent_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Auth_entity_ID")]
-                        aut_com_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Auth_comp_ID")]
-                        aut_seq_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Auth_seq_ID")]
-                        aut_atm_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Auth_atom_ID")]   
+                        if "Entity_assembly_ID" in assigned_peak.columns: ent_asm_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Entity_assembly_ID")]
+                        if "Entity_ID" in assigned_peak.columns: ent_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Entity_ID")]
+                        if "Comp_ID" in assigned_peak.columns: com_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Comp_ID")] 
+                        if "Comp_index_ID" in assigned_peak.columns: com_ind_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Comp_index_ID")]
+                        if "Atom_ID" in assigned_peak.columns: atm_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Atom_ID")]
+                        if "Auth_entity_assembly_ID" in assigned_peak.columns: aut_ent_asm_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Auth_entity_assembly_ID")]
+                        if "Auth_entity_ID" in assigned_peak.columns: aut_ent_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Auth_entity_ID")]
+                        if "Auth_comp_ID" in assigned_peak.columns: aut_com_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Auth_comp_ID")]
+                        if "Auth_seq_ID" in assigned_peak.columns: aut_seq_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Auth_seq_ID")]
+                        if "Auth_atom_ID" in assigned_peak.columns: aut_atm_id[(dat[assigned_peak.columns.index("Peak_ID")],dat[assigned_peak.columns.index("Spectral_dim_ID")])]= dat[assigned_peak.columns.index("Auth_atom_ID")]   
                 new_peak_loop = pynmrstar.Loop.from_scratch(peak.category)   
                 for col_name in peak.columns:
                     new_peak_loop.add_column(col_name)
                 for dat in peak.data:
                     new_dat = copy.deepcopy(dat)
-                    try:
-                        new_dat.append(measurement[(dat[peak.columns.index("ID")],"height")])
-                    except KeyError:
-                        new_dat.append(".")
-                    try:
-                        new_dat.append(measurement_err[(dat[peak.columns.index("ID")],"height")])
-                    except KeyError:
-                        new_dat.append(".")
-                    try:
-                        new_dat.append(measurement[(dat[peak.columns.index("ID")],"volume")])
-                    except KeyError:
-                        new_dat.append(".")
-                    try:
-                        new_dat.append(measurement_err[(dat[peak.columns.index("ID")],"volume")])
-                    except KeyError:
-                        new_dat.append(".")
+                    if "_Peak_general_char" in self.loop_category:
+                        if "height" in peak_gen_char.get_data_by_tag("Measurement_method")[0]:
+                            try:
+                                new_dat.append(measurement[(dat[peak.columns.index("ID")],"height")])
+                            except KeyError:
+                                new_dat.append(".")
+                            try:
+                                new_dat.append(measurement_err[(dat[peak.columns.index("ID")],"height")])
+                            except KeyError:
+                                new_dat.append(".")
+                        if "volume" in peak_gen_char.get_data_by_tag("Measurement_method")[0]:
+                            try:
+                                new_dat.append(measurement[(dat[peak.columns.index("ID")],"volume")])
+                            except KeyError:
+                                new_dat.append(".")
+                            try:
+                                new_dat.append(measurement_err[(dat[peak.columns.index("ID")],"volume")])
+                            except KeyError:
+                                new_dat.append(".")
                     for d in range(self.dim):
                         try:
                             new_dat.append(position[(dat[peak.columns.index("ID")],"%d"%(d+1))])
@@ -191,53 +230,56 @@ class NMRSTAR(object):
                             new_dat.append(position_err[(dat[peak.columns.index("ID")],"%d"%(d+1))])
                         except KeyError:
                             new_dat.append(".")
-                    for d in range(self.dim):
-                        try:
-                            new_dat.append(ent_asm_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
-                        except KeyError:
-                            new_dat.append(".")
-                    for d in range(self.dim):
-                        try:
-                            new_dat.append(ent_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
-                        except KeyError:
-                            new_dat.append(".")
-                    for d in range(self.dim):
-                        try:
-                            new_dat.append(com_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
-                        except KeyError:
-                            new_dat.append(".")
-                    for d in range(self.dim):
-                        try:
-                            new_dat.append(com_ind_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
-                        except KeyError:
-                            new_dat.append(".")
-                    for d in range(self.dim):
-                        try:
-                            new_dat.append(atm_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
-                        except KeyError:
-                            new_dat.append(".")
-                    for d in range(self.dim):
-                        try:
-                            new_dat.append(aut_ent_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
-                        except KeyError:
-                            new_dat.append(".")
-                    for d in range(self.dim):
-                        try:
-                            new_dat.append(aut_com_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
-                        except KeyError:
-                            new_dat.append(".")
-                    for d in range(self.dim):
-                        try:
-                            new_dat.append(aut_seq_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
-                        except KeyError:
-                            new_dat.append(".")
-                    for d in range(self.dim):
-                        try:
-                            new_dat.append(aut_atm_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
-                        except KeyError:
-                            new_dat.append(".")
+                    if "_Assigned_peak_chem_shift" in self.loop_category:
+                        for d in range(self.dim):
+                            try:
+                                new_dat.append(ent_asm_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
+                            except KeyError:
+                                new_dat.append(".")
+                        for d in range(self.dim):
+                            try:
+                                new_dat.append(ent_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
+                            except KeyError:
+                                new_dat.append(".")
+                        for d in range(self.dim):
+                            try:
+                                new_dat.append(com_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
+                            except KeyError:
+                                new_dat.append(".")
+                        for d in range(self.dim):
+                            try:
+                                new_dat.append(com_ind_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
+                            except KeyError:
+                                new_dat.append(".")
+                        for d in range(self.dim):
+                            try:
+                                new_dat.append(atm_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
+                            except KeyError:
+                                new_dat.append(".")
+                        for d in range(self.dim):
+                            try:
+                                new_dat.append(aut_ent_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
+                            except KeyError:
+                                new_dat.append(".")
+                        for d in range(self.dim):
+                            try:
+                                new_dat.append(aut_com_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
+                            except KeyError:
+                                new_dat.append(".")
+                        for d in range(self.dim):
+                            try:
+                                new_dat.append(aut_seq_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
+                            except KeyError:
+                                new_dat.append(".")
+                        for d in range(self.dim):
+                            try:
+                                new_dat.append(aut_atm_id[(dat[peak.columns.index("ID")],"%d"%(d+1))])
+                            except KeyError:
+                                new_dat.append(".")
                     #print new_peak_loop.columns
                     #print new_dat   
+                    #print new_peak_loop.columns
+                    #print new_dat
                     new_peak_loop.add_data(new_dat)
                 outSaveframe.add_loop(new_peak_loop)
                 self.outputStar.add_saveframe(outSaveframe)
@@ -246,7 +288,8 @@ class NMRSTAR(object):
                 self.outputStar.add_saveframe(saveframe)
         #print self.outputStar
         #pynmrstar.Entry.normalize(self.outputStar, schema = self.newSchema)
-        #self.outputStar.normalize()
+        self.outputStar.normalize()
+        self.outputStar.normalize()
         with open(self.outfile,'w') as wstarfile:
             wstarfile.write(str(self.outputStar))
         
@@ -258,7 +301,14 @@ class NMRSTAR(object):
 if __name__=="__main__":
     #p = NMRSTAR('/home/kumaran/git/nmr-star-dictionary/bmrb_star_v3_files/adit_input/nmrstar3_fake.txt')
     #p = NMRSTAR('/home/kumaran/git/nmr-star-dictionary/bmrb_only_files/adit_input/nmrstar3_fake.txt')
-    p=NMRSTAR('26860')
-    p.Convert()
+    f=open('/home/kumaran/git/NMR-STAR-Converter/uniq_peak.dat','r')
+    #p=NMRSTAR("26860")
+    #p=NMRSTAR("15090")
+    #p.Convert()
+    for l in f:
+        bmrbid = l.split("\n")[0]
+        p=NMRSTAR(bmrbid)
+        p.Convert()
+        
     
       
